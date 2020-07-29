f9b65587df5a4f3cbb21f7334dcb341e9bb5743e
Aaron Giera <aaron.giera@va.gov>
2020-07-29 08:51:33 -0400

    fix run sql file

--- ci/run_sql.sh
+++ ci/run_sql.sh
@@ -1,2 +1,2 @@
-docker exec -it $(docker ps -qf name=proposalmgr_mysql-db) mysql -u dbuser -pdbuser#1812 Proposals_MRS -e "$(cat $1)"
+cat $1 | docker exec -i $(docker ps -qf name=proposalmgr_mysql-db) mysql -u dbuser -pdbuser#1812 Proposals_MRS
 
